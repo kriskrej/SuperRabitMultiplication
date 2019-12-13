60c9f1f6b9b75ece3f655616dc82f281f3f62867
kris <kris@krej.net>
2019-12-13 14:30:29 +0100

    Joining the room

---
+++ Assets/Code/NetworkConnector.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkConnector : MonoBehaviourPunCallbacks {
+    void Awake() {
+        // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    void Start() {
+        Connect();
+    }
+
+
+    void Connect() {
+        if (PhotonNetwork.IsConnected)
+            PhotonNetwork.JoinRandomRoom();
+        else
+            PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster() {
+        Debug.Log("OnConnectedToMaster() was called by PUN");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+
+    public override void OnDisconnected(DisconnectCause cause) {
+        Debug.LogWarningFormat("OnDisconnected() was called by PUN with reason {0}",
+            cause);
+    }
+
+    public override void OnJoinedRoom() {
+        Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message) {
+        Debug.Log(
+            "OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+        PhotonNetwork.CreateRoom(null, new RoomOptions());
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class GameManager : MonoBehaviourPunCallbacks {
+    public override void OnPlayerEnteredRoom(Player other) {
+        Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
+
+        if (PhotonNetwork.IsMasterClient) {
+            Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}",
+                PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+        }
+    }
+
+
+    public override void OnPlayerLeftRoom(Player other) {
+        Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
+
+        if (PhotonNetwork.IsMasterClient) {
+            Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}",
+                PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+        }
+    }
+
+
+    public override void OnLeftRoom() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LeaveRoom() {
+        PhotonNetwork.LeaveRoom();
+    }
+}
