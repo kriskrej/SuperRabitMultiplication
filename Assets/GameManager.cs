77d88b36abb41c6842bd7650b5ca7e8020807509
kris <kris@krej.net>
2019-12-13 14:37:12 +0100

    rabbit spawning

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,6 +4,12 @@ using Photon.Realtime;
 using UnityEngine;
 
 public class GameManager : MonoBehaviourPunCallbacks {
+    public GameObject playerPrefab;
+
+    void Start() {
+        PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0f,5f,0f), Quaternion.identity, 0);
+    }
+    
     public override void OnPlayerEnteredRoom(Player other) {
         Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
 
